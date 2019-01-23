27692f1fd449ff33b7c99b0458a92c87e06bbee3
Freddy Tuxworth <freddytuxworth@gmail.com>
2019-01-23 00:27:51 +0000

    Functional updates

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require('electron-css-reload')(250);
+var temp = require('temp'),
+    fs   = require('fs');
+
+temp.track();
 
 var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
     //various options - see CodeMirror docs
@@ -18,8 +22,26 @@ function execute(command, callback) {
 var runButton = document.getElementById("runButton");
 var output = document.getElementById("output");
 runButton.onclick = function(){
-    exec("echo bean", (error, stdout, stderr) => { 
-        output.innerHTML = "";
-        output.innerHTML += stdout;
+    runButton.classList.add("working");
+    runButton.innerHTML = "saving";
+    temp.open('jester_java_code', function(err, javatmp) {
+        if (!err) {
+            fs.write(javatmp.fd, editor.getValue(), function(){
+                fs.close(javatmp.fd, function(err) {
+                    runButton.innerHTML = "compiling";
+                    exec("type " + javatmp.path, function(err, stdout) {
+                        runButton.innerHTML = "RUN";
+                        runButton.classList.remove("working");
+                        if(err){
+                            output.classList.add("error");
+                            output.innerHTML = err;
+                        } else {
+                            output.classList.remove("error");
+                            output.innerHTML = stdout;
+                        }
+                    });
+                })
+            });
+        }
     });
 }
